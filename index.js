bb1a8249bf7a3839159500539577982950cfa019
Aakash <87240223+reddyakash290@users.noreply.github.com>
2023-07-03 01:34:35 +0530

    added index.js file and modified style.css

---
+++ index.js
@@ -0,0 +1,12 @@
+var hiddenBox = $(".events-ch");
+
+$(".events-btn").click(function () {
+  $(hiddenBox).toggle();
+  hiddenBox.css("display", "grid");
+
+  if (
+    $(".events-btn").click(function () {
+      $(hiddenBox).toggle();
+    })
+  );
+});
